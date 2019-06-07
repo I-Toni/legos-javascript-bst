82381587e82ab8a695e59fb98cc0cd053a33ac23
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2019-06-07 18:02:10 -0400

    edited testBrickConstructor to add size test and modified description name

--- test/testBrickConstructor.js
+++ test/testBrickConstructor.js
@@ -4,9 +4,13 @@ const Brick = lego.Brick;
 
 var headBrick = new Brick(4, 'blue');
 
-describe('Brick', function(){
+describe('HeadBrick Color and Size', function(){
   /**color is blue*/
   it('Should be a blue color', function() {
     assert.equal('blue', headBrick.color);
+  });
+  /**size is 4*/
+  it('Should be size 4', function() {
+    assert.equal(4, headBrick.size);
   });
 });
