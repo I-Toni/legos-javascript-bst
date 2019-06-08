de8363dc981616c14a656776c545f9a43542f9d0
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2019-06-08 17:37:42 -0400

    removed inOrder in bst.js and added assertion test to testBstInser.js

--- test/testBstInsert.js
+++ test/testBstInsert.js
@@ -4,3 +4,17 @@ var bst = require('../bst');
 
 const Brick = lego.Brick;
 const BST = bst.BST;
+
+var bst = new BST();
+var brick0 = new Brick(4, 'blue');
+
+bst.insert(brick0);
+/**get BSTs root node via root field,
+  then data field should be the same as the brick object
+*/
+describe('Add brick to BST', function(){
+  /**4 as root node*/
+  it('brick0 should have same data as root', function() {
+    assert.equal(bst.root.data, brick0);
+  });
+});
