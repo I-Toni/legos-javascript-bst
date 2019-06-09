8f42c28c21842d15e54c364cfd4243a50c0f4795
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2019-06-09 12:11:32 -0400

    edited test to add a single brick to a BST

--- test/testBstInsert.js
+++ test/testBstInsert.js
@@ -5,16 +5,45 @@ var bst = require('../bst');
 const Brick = lego.Brick;
 const BST = bst.BST;
 
-var bst = new BST();
-var brick0 = new Brick(4, 'blue');
 
-bst.insert(brick0);
-/**get BSTs root node via root field,
+
+describe('Add brick0 to BST', function(){
+  /**get BSTs root node via root field,
   then data field should be the same as the brick object
-*/
-describe('Add brick to BST', function(){
-  /**4 as root node*/
+  */
   it('brick0 should have same data as root', function() {
+    var bst = new BST();
+    var brick0 = new Brick(4, 'blue');
     assert.equal(bst.root.data, brick0);
   });
 });
+
+var brick1 = new Brick(2, 'blue');
+
+/**
+describe('Add brick0 brick1 to BST', function(){
+  /**root plus a left child
+  it('brick0 should have same data as left child of root', function() {
+    assert.equal(bst.root.left, brick1);
+  });
+});
+
+var brick2 = new Brick(5, 'blue');
+describe('Add brick0 and brick2 to BST', function(){
+  /**root plus a right child
+  it('brick2 should have same data as right child of root', function() {
+    assert.equal(bst.root.right, brick2);
+  });
+});
+ 
+describe('Add brick0, brick1 and brick2 to BST', function(){
+  /**root plus a left and right child
+  it('brick1 and brick2 should have same data as left and right child of root', function() {
+    assert.equal(bst.root.left, brick1);
+    assert.equal(bst.root.right, brick2);
+  });
+});
+
+*/
+*/
+
