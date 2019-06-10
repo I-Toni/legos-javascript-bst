6c810da3754bbabdf7b1c161542ae071880a9624
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2019-06-10 19:11:03 -0400

    added legoPile constructor to legos.js

--- legos.js
+++ legos.js
@@ -1,6 +1,13 @@
 module.exports.Brick = Brick;
+module.exports.LegoPile = LegoPile;
 
 function Brick(size, color) {
   this.size = size;
   this.color = color;
+}
+
+
+function LegoPile() {
+  this.dict = dict;
+  this.insert = insert;
 }
