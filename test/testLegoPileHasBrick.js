93bab3e13471b8174c4f65aa2788755d0faa02c8
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2019-06-12 01:29:30 -0400

    deleted testLegoPileHasBrick test file

--- test/testLegoPileHasBrick.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-var lego = require('../legos');
-var bst = require('../bst');
-
-const Brick = lego.Brick;
-const LegoPile = lego.LegoPile;
-const BST = bst.BST;
-
-
