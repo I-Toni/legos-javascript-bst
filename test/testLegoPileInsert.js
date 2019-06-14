c44507d679d7bec80365a948d7318b491a99810b
Ibrahim Tonifarah <itonifa1@my.westga.edu>
2019-06-14 05:55:09 -0400

    edited testLegoPileInsert.js by separating assertions into different test and modifying descriptions

--- test/testLegoPileInsert.js
+++ test/testLegoPileInsert.js
@@ -16,6 +16,7 @@ describe('Insert one brick into the Appropriate BST', function(){
   });
 });
 
+/**Insert one brick of each into BST*/
 var legoPile2 = new LegoPile();
 var brick10 = new Brick(4, 'red');
 var brick1 = new Brick(4, 'green');
@@ -24,33 +25,48 @@ var brick3 = new Brick(4, 'yellow');
 var brick4 = new Brick(4, 'black');
 var brick5 = new Brick(4, 'white');
 describe('Insert one brick of each color into the appropriate BST', function(){
-  it('brick10, brick1, brick2, brick3, brick4, brick5 should have same data as root of red,green, blue, yellow, black, and white dictionary key,respectively.', function() {
+  it('Should have red brick as root of red BST', function() {
     legoPile2.insert(brick10);
-    legoPile2.insert(brick1);
-    legoPile2.insert(brick2);
-    legoPile2.insert(brick3);
-    legoPile2.insert(brick4);
-    legoPile2.insert(brick5);
     assert.equal(legoPile2.dict["red"].root.data, brick10);
+  });
+  it('Should have green brick as root of green BST', function() {
+    legoPile2.insert(brick1);
     assert.equal(legoPile2.dict["green"].root.data, brick1);
+  });
+  it('Should have blue brick as root of blue BST', function() {
+  legoPile2.insert(brick2);
     assert.equal(legoPile2.dict["blue"].root.data, brick2);
+  });
+  it('Should have yellow brick as root of yellow BST', function() {
+    legoPile2.insert(brick3);
     assert.equal(legoPile2.dict["yellow"].root.data, brick3);
+  });
+  it('Should have black brick as root of black BST', function() {
+    legoPile2.insert(brick4);
     assert.equal(legoPile2.dict["black"].root.data, brick4);
+  });
+  it('Should have white brick as root of white BST', function() {
+    legoPile2.insert(brick5);
     assert.equal(legoPile2.dict["white"].root.data, brick5);
   });
 });
 
+/**Insert several bricks of red color into BST*/
 var legoPile3 = new LegoPile();
 var brick100 = new Brick(4, 'red');
 var brick200 = new Brick(3, 'red');
 var brick300 = new Brick(5, 'red');
 describe('Insert several bricks of red into the appropriate BST', function(){
-  it('brick100 should be root of red, brick200 should be in left node of root, brick300 should be in right node of root', function() {
+  it('should be root of red', function() {
     legoPile3.insert(brick100);
-    legoPile3.insert(brick200);
-    legoPile3.insert(brick300);
     assert.equal(legoPile3.dict["red"].root.data, brick100);
+  });
+  it('should be in left node of root', function() {
+    legoPile3.insert(brick200);
     assert.equal(legoPile3.dict["red"].root.left.data, brick200);
+  });
+  it('should be in right node of root', function() {
+    legoPile3.insert(brick300);
     assert.equal(legoPile3.dict["red"].root.right.data, brick300);
   });
 });
@@ -91,7 +107,7 @@ describe('Test BST count returns appropriate number of node objects in BST', fun
 
 /**Test LegoPile#count returns the total number of node objects in the legoPile*/
 describe('Test LegoPile count returns appropriate total number of node objects in LegoPile', function(){
-  it('(legoPile2) Should return 7 after inserting 7 bricks with different colors and calling count method', function() {
+  it('(legoPile2) Should return 6 after inserting 6 bricks with different colors and calling count method', function() {
     assert.equal(legoPile2.count(), 6);
   });
   it('(legoPile3) Should return 3 after inserting 3 bricks with different colors and calling count method', function() {
